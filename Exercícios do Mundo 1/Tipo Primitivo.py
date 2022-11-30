d2dddef81151ca7a7c1a51151c6027e3e92cb3c1
Marcelo Martins <93449600+2MBG@users.noreply.github.com>
2022-11-30 09:28:11 -0300

    Update Tipo Primitivo.py

--- Exercícios do Mundo 1/Tipo Primitivo.py
+++ Exercícios do Mundo 1/Tipo Primitivo.py
@@ -1,4 +1,4 @@
-# type diz qual o tipo primitivo da variável
+# Caso você não tenha certeza qual é o tipo de um valor, pode usar a função type() para checar
 a = int (input ('\nDigite um valor: '))
 
 print ('O tipo pimitivo desse valor é', type(a))
