72a4a3c10ab9acade3580fbaf5a19cb968975fbb
Marcelo Martins <93449600+2MBG@users.noreply.github.com>
2023-03-25 18:05:04 -0300

    Update Soma dos Pares.py

--- Exercícios do Mundo 2/Soma dos Pares.py
+++ Exercícios do Mundo 2/Soma dos Pares.py
@@ -5,7 +5,7 @@ contator = 0
 
 for contagem in range(1, 7):
     
-    num = int(input(f'Digite o {contagem} valor: '))
+    num = int(input(f'Digite o {contagem}° valor: '))
     
     if num % 2 == 0:
         soma += num
